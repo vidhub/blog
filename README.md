8e09fa77848f8cee061618296ecef796b4419eb7
Adam Zmenak <adam@dinteractive.ca>
2014-05-17 19:02:46 -0400

    Some more setup

---
+++ README.md
@@ -0,0 +1,13 @@
+# Yay Vidhub blog!
+
+## Running directions
+1. Install `gulp` if you don't already have it
+    sudo npm install -g gulp
+2. Install the deps
+    npm install
+3. Run the dev server
+    gulp
+
+## Deploy
+Just run
+    gulp deploy
---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp      = require('gulp'),
+watch         = require('gulp-watch'),
+uglify        = require('gulp-uglify'),
+concat        = require('gulp-concat'),
+coffee        = require('gulp-coffee'),
+spawn         = require('gulp-spawn'),
+autoprefixer  = require('gulp-autoprefixer'),
+livereload    = require('gulp-livereload'),
+clean         = require('gulp-clean'),
+cache         = require('gulp-cache'),
+notify        = require('gulp-notify'),
+deploy        = require('gulp-gh-pages'),
+imagemin      = require('gulp-imagemin');
+
+gulp.task('default', function() {
+  spawn({
+    cmd: 'jekyll serve',
+    args: '--watch'
+  });
+
+  var server = livereload()
+  gulp.watch('_site/**')
+      .on('change', function(file) {
+        server.changed(file.path)
+      });
+});
+
+gulp.task('deploy', function() {
+  gulp.src('./_site/**/*')
+      .pipe(deploy({
+        remoteUrl: 'https://github.com/vidhub/blog'
+      }));
+});
